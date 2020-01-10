a778bcb93b01cab4ff6e800a384b74e939008c93
liulei33 <liulei33@58.com>
2020-01-10 11:52:22 +0800

    增加spring-boot-jdbc   jar，利用springboot自动装配在测试类中注入jdbctemplate，并测试是否能连接到mysql正常使用

--- springboot-starter-demo/src/test/java/com/assembly/springboot/starter/SpringbootStarterDemoApplicationTests.java
+++ springboot-starter-demo/src/test/java/com/assembly/springboot/starter/SpringbootStarterDemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.assembly.springboot.starter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class SpringbootStarterDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @Test
+    public void addData(){
+        String sql = "insert into user (name,age) values ('Mr.liu',18)";
+        jdbcTemplate.execute(sql);
+    }
 }
